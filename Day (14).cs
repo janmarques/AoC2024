42d8326188bb05e4d855c9a8de737d23b5c1a93b
Jan Marques <Jan.Marques@mediahuis.be>
2024-12-13 08:31:29 +0100

    w

--- Day (14).cs
+++ Day (14).cs
@@ -7,15 +7,18 @@
 //var smallest = "";
 
 //var input = smallInput;
-////var input = fullInput;
-////var input = smallest;
+////input = fullInput;
+////input = smallest;
+//var timer = System.Diagnostics.Stopwatch.StartNew();
 
-//var result = "";
+//var result = 0l;
 
 //foreach (var line in input.Split(Environment.NewLine))
 //{
 
 //}
 
+//timer.Stop();
 //Console.WriteLine(result);
+//Console.WriteLine(timer.ElapsedMilliseconds + "ms");
 //Console.ReadLine();
