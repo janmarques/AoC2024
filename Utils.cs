32648698bf994e4dab72f980712b0baa9309ec18
Jan Marques <jan@janmarques.net>
2024-12-18 10:02:00 +0100

    w

--- Utils.cs
+++ Utils.cs
@@ -6,7 +6,33 @@ using System.Threading.Tasks;
 
 namespace AoC2024s
 {
-    internal class Utils
+    public class Utils
     {
+        public void PrintGrid<T>(T[][] grid, Func<T, string> print)
+        {
+            for (int i = 0; i < grid.Length; i++)
+            {
+                for (int j = 0; j < grid[i].Length; j++)
+                {
+                    Console.Write(print(grid[i][j]));
+                }
+                Console.WriteLine();
+            }
+        }
+
+        public void PrintGrid2<T>(IEnumerable<T> grid, Func<T, int> X, Func<T, int> Y, Func<T, string> print)
+        {
+            var width = grid.Max(X);
+            var height = grid.Max(Y);
+            for (int i = 0; i <= width; i++)
+            {
+                for (int j = 0; j <= height; j++)
+                {
+                    var item = grid.SingleOrDefault(o => X(o) == j && Y(o) == i);
+                    Console.Write(item is null ? "?" : print(item));
+                }
+                Console.WriteLine();
+            }
+        }
     }
 }
